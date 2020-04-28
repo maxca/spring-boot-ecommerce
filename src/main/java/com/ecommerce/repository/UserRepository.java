e5b51385c03a6131dbd421da4283e0f7c9408548
samark chisangun <samark.chaisangun@scb.co.th>
2020-04-28 17:50:28 +0700

    add feature user login

--- src/main/java/com/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.ecommerce.repository;
 
 import com.ecommerce.model.User;
+import com.ecommerce.model.request.UserLoginRequest;
+import com.ecommerce.repository.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -31,5 +34,36 @@ public class UserRepository {
         params.put("name", user.getName());
         params.put("phone", user.getPhone());
         return namedParameterJdbcTemplate.update(sql.toString(), params);
+    }
+
+    public User login(UserLoginRequest request) {
+        StringJoiner sql = new StringJoiner(" ");
+        sql.add("select id ,email, phone ,name from users")
+                .add("where users.email =:email")
+                .add("and users.password =:password;");
+        HashMap<String, Object> params = new HashMap<>();
+
+        params.put("email", request.getEmail());
+        params.put("password", request.getPassword());
+        try {
+            return namedParameterJdbcTemplate.queryForObject(sql.toString(), params, new UserMapper());
+        } catch (EmptyResultDataAccessException ex) {
+            log.error("not found user");
+            return null;
+        }
+    }
+
+    public Object updateLastLogin(String userId) {
+        StringJoiner sql = new StringJoiner(" ");
+        sql.add("update users set")
+                .add("last_login = now()")
+                .add("where id =:userId");
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        try {
+            return namedParameterJdbcTemplate.update(sql.toString(), params);
+        } catch (Exception ex) {
+            return null;
+        }
     }
 }
--- src/main/java/com/ecommerce/repository/UserSessionRepository.java
+++ src/main/java/com/ecommerce/repository/UserSessionRepository.java
@@ -5,11 +5,13 @@ import com.ecommerce.repository.mapper.UserSessionMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.repository.query.QueryCreationException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.StringJoiner;
+import java.util.UUID;
 
 @Repository
 @Slf4j
@@ -34,6 +36,23 @@ public class UserSessionRepository {
             log.error("select user session query :", sql.toString());
             return null;
         }
+    }
 
+    public String createUserSession(String userId) {
+        StringJoiner sql = new StringJoiner(" ");
+        sql.add("INSERT INTO users_session(")
+                .add("id, user_id, expired_datetime, created_datetime, updated_datetime)")
+                .add("VALUES (:sessionId, :userId, now() +'1 hours'::interval, now(), now());");
+        HashMap<String, Object> params = new HashMap<>();
+        String sessionId = UUID.randomUUID().toString();
+        params.put("sessionId", sessionId);
+        params.put("userId", userId);
+        try {
+            namedParameterJdbcTemplate.update(sql.toString(), params);
+            return sessionId;
+        } catch (QueryCreationException ex) {
+            log.error("query error", ex.getMessage());
+            return null;
+        }
     }
 }
